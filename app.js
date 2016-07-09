d3508093c4a343f05b9340baad0eaa4510d7c58a
Norman Croan <ncroan@gmail.com>
2016-07-08 21:51:01 -0400

    refactoring

--- app.js
+++ app.js
@@ -14,51 +14,68 @@ serv.listen(app.get('port'), function(){
 });
 
 var SOCKET_LIST = {};
-var PLAYER_LIST = {};
 
-var Player = function(id) {
+var Entity = function(){
   var self = {
     x:250,
     y:250,
-    id:id,
-    number: "" + Math.floor(10 * Math.random()),
-    pressingRight:false,
-    pressingLeft:false,
-    pressingUp:false,
-    pressingDown:false,
-    maxSpd:10
+    spdX:0,
+    spdY:0,
+    id:"",
   }
   self.updatePosition = function(){
-    if (self.pressingRight) {
-      self.x += self.maxSpd;
-    }
-    if (self.pressingLeft) {
-      self.x -= self.maxSpd;
-    }
-    if (self.pressingUp) {
-      self.y -= self.maxSpd;
-    }
-    if (self.pressingDown) {
-      self.y += self.maxSpd;
-    }
+    self.x += self.spdX;
+    self.y += self.spdY;
+  }
+  self.update = function(){
+    self.updatePosition();
   }
   return self;
 }
 
-var io = require('socket.io') (serv,{});
+var Player = function(id) {
+  var self = Entity();
+  self.id = id;
+  self.number = "" + Math.floor(10 * Math.random());
+  self.pressingRight = false;
+  self.pressingLeft = false;
+  self.pressingUp = false;
+  self.pressingDown = false;
+  self.maxSpd = 10;
 
-io.sockets.on('connection', function(socket){
-  socket.id = Math.random();
-  SOCKET_LIST[socket.id] = socket;
+  self.updateSpd = function(){
+    if (self.pressingRight) {
+      self.spdX = self.maxSpd;
+    }
+    else if (self.pressingLeft) {
+      self.spdX = -self.maxSpd;
+    }
+    else {
+      self.spdX = 0;
+    }
 
+    if (self.pressingUp) {
+      self.spdY = -self.maxSpd;
+    }
+    else if (self.pressingDown) {
+      self.spdY = self.maxSpd;
+    }
+    else {
+      self.spdY = 0;
+    }
+  }
+
+  var super_update = self.update;
+  self.update = function(){
+    self.updateSpd();
+    super_update();
+  }
+  Player.list[id] = self;
+  return self;
+}
+Player.list = {};
+Player.onConnect = function(socket){
   var player = Player(socket.id);
-  PLAYER_LIST[socket.id] = player;
-
-  socket.on('disconnect', function(){
-    delete SOCKET_LIST[socket.id];
-    delete PLAYER_LIST[socket.id];
-  });
-
   socket.on('keyPress', function(data){
     if (data.inputID === 'left') {
       player.pressingLeft = data.state;
@@ -73,21 +90,40 @@ io.sockets.on('connection', function(socket){
       player.pressingDown = data.state;
     }
   });
-
-});
-
-
-setInterval(function(){
+}
+Player.onDisconnect = function(socket){
+  delete Player.list[socket.id];
+}
+Player.update = function(){
   var pack = [];
-  for(var i in PLAYER_LIST){
-    var player = PLAYER_LIST[i];
-    player.updatePosition();
+  for(var i in Player.list){
+    var player = Player.list[i];
+    player.update();
     pack.push({
       x:player.x,
       y:player.y,
       number: player.number
     });
   }
+  return pack;
+}
+
+var io = require('socket.io') (serv,{});
+
+io.sockets.on('connection', function(socket){
+  socket.id = Math.random();
+  SOCKET_LIST[socket.id] = socket;
+
+  Player.onConnect(socket);
+  socket.on('disconnect', function(){
+    delete SOCKET_LIST[socket.id];
+    Player.onDisconnect(socket);
+  });
+});
+
+
+setInterval(function(){
+  var pack = Player.update();
   for(var i in SOCKET_LIST){
   var socket = SOCKET_LIST[i];
   socket.emit('newPositions', pack);
